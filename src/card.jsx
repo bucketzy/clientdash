fc16e647bf97392d642aeef4559252049c45a2f4
bucketzy <81269291+bucketzy@users.noreply.github.com>
2025-06-29 17:11:52 -0700

    Add files via upload

---
+++ src/card.jsx
@@ -0,0 +1,2 @@
+export const Card = ({ children }) => <div className='border rounded-md bg-white shadow'>{children}</div>;
+export const CardContent = ({ children }) => <div className='p-4'>{children}</div>;
