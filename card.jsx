2a9f4d15adedbfc95544d3993c5b1b585cb630f5
bucketzy <81269291+bucketzy@users.noreply.github.com>
2025-06-29 17:12:27 -0700

    Delete card.jsx

--- card.jsx
+++
@@ -1,2 +0,0 @@
-export const Card = ({ children }) => <div className='border rounded-md bg-white shadow'>{children}</div>;
-export const CardContent = ({ children }) => <div className='p-4'>{children}</div>;
